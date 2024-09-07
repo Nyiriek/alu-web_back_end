f2b682b9e535e0361fc292ea795cc547c49f0c4e
Nyiriek <n.peat@alustudent.com>
2024-09-07 16:43:02 +0200

    Add changes

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-"""Async generator"""
+"""
+The coroutine will loop 10 times, each
+time asynchronously wait 1 second, then yield arandom number between 0 and 10. Use the random module.
+"""
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
-    The coroutine will loop 10 times, each
-    time asynchronously wait 1 second, then yield a
-    random number between 0 and 10. Use the random module.
+    Async Generator
+
+    Yields:
+        float: A random float number between 0 and 10.
     """
     for i in range(10):
         await asyncio.sleep(1)
