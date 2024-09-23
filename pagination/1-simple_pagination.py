3784320b7fb439c728fd32768fb06fdae80a2c6d
Nyiriek <n.peat@alustudent.com>
2024-09-23 14:39:49 +0200

    Simple Pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -12,6 +12,7 @@ def index_range(page: int, page_size: int) -> tuple:
     """Return a tuple of size two containing a start index and an end index"""
     return ((page - 1) * page_size, page * page_size)
 
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
