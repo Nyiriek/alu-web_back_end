77e64a48dfa7c9f6e24c02a79a914c03caf582ab
Nyiriek <n.peat@alustudent.com>
2024-09-23 14:50:12 +0200

    Add changes

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -42,23 +42,24 @@ class Server:
         return self.dataset()[start:end]
 
 
-    def get_hyper(page: int, page_size: int) -> dict:
-        """Return a dictionary containing the following key-value pairs:
-        - page_size: the length of the returned dataset page
-        - page: the current page number
-        - data: the dataset page (equivalent to return from previous task)
-        - next_page: number of the next page, None if no next page
-        - prev_page: number of the previous page, None if no previous page
-        - total_pages: the total number of pages in the dataset as an integer
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """get hyper
+
+        Args:
+            page (int, optional): current page. Defaults to 1.
+            page_size (int, optional):
+            Number of elements in page. Defaults to 10.
+
+        Returns:
+            Dict: Dictonary of pagination elements
         """
-        server = Server()
-        data = server.get_page(page, page_size)
-        total_pages = math.ceil(len(server.dataset()) / page_size)
-        return {
-        "page_size": len(data),
-        "page": page,
-        "data": data,
-        "next_page": page + 1 if page < total_pages else None,
-        "prev_page": page - 1 if page > 1 else None,
-        "total_pages": total_pages
-    }
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        new_dictionary = {
+            "page_size": page_size,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
+        return new_dictionary
