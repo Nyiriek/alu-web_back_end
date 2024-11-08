4db9906def7b67cf5f732d6bd2f8e26bf01ead35
Nyiriek <n.peat@alustudent.com>
2024-11-09 00:14:45 +0200

    fix error

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
+  const view = new DataView(buffer);
 
   try {
     view.setInt8(position, value);
