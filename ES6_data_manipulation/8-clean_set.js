ab14bce1f4480ce08be055d8d6ec9732f03cdc6d
Nyiriek <n.peat@alustudent.com>
2024-11-09 00:38:33 +0200

    fix error

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
-    return '';
-  }
-  const array = Array.from(set);
-  const filteredArray = array.filter((string) => string.startsWith(startString));
-  return filteredArray.join('-');
+  let result = '';
+  if (!startString || !startString.length) return result;
+  set.forEach((i) => {
+    if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
+  });
+  return result.slice(0, result.length - 1);
 }
